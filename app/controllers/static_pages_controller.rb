0451e8f465234b25f5ddf15e9218c7d198bcc934
Test User <testuser4213@zoho.com>
2016-02-26 15:53:48 +0200

    slkdfjslafj

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
+  layout 'empty', only: [:home]
+
   # GET /
   def home
+    @latest_tuts = Post.order(created_at: :desc).limit(5)
   end
 
   # GET /about
